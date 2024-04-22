62ace034b1a38624e4bdab275ced87251ecdc0bd
Nikola Samardžić <nikola.samardzic.it@gmail.com>
2024-04-22 09:32:10 +0200

    Add Publisher Entity and its configuration

---
+++ ReadilyAPI.DataAccess/Configurations/PublisherConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReadilyAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadilyAPI.DataAccess.Configurations
+{
+    internal class PublisherConfiguration : NamedEntityConfiguration<Publisher>
+    {
+        protected override void ConfigureEntity(EntityTypeBuilder<Publisher> builder)
+        {
+
+        }
+    }
+}
--- ReadilyAPI.DataAccess/ReadilyContext.cs
+++ ReadilyAPI.DataAccess/ReadilyContext.cs
@@ -37,5 +37,6 @@ namespace ReadilyAPI.DataAccess
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
     }
 }
