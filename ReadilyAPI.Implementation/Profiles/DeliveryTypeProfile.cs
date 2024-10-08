a52335f1f001942602137c6ec40975882541e97a
Nikola Samardžić <nikola.samardzic.it@gmail.com>
2024-10-08 13:30:45 +0200

    Add Automapper profile for delivery type

---
+++ ReadilyAPI.Implementation/Profiles/DeliveryTypeProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ReadilyAPI.Application.UseCases.DTO.DeliveryType;
+using ReadilyAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadilyAPI.Implementation.Profiles
+{
+    public class DeliveryTypeProfile : Profile
+    {
+        public DeliveryTypeProfile() 
+        {
+            CreateMap<CreateDeliveryTypeDto, DeliveryType>();
+
+            CreateMap<UpdateDeliveryTypeDto, DeliveryType>();
+
+            CreateMap<DeliveryType, DeliveryTypeDto>()
+                .ForMember(d => d.OrdersCount, s => s.MapFrom(x => x.Orders.Count()));
+        }
+    }
+}
