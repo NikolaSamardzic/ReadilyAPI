aca44a48ab02280ee04972415281b3cb76639e4f
Nikola Samardžić <nikola.samardzic.it@gmail.com>
2024-10-10 22:08:37 +0200

    Add automapper profile for wishlist

--- ReadilyAPI.Implementation/UseCases/Commands/Wishlist/EfCreateWishlistCommand.cs
+++ ReadilyAPI.Implementation/UseCases/Commands/Wishlist/EfCreateWishlistCommand.cs
@@ -1,10 +1,12 @@
-﻿using FluentValidation;
+﻿using AutoMapper;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using ReadilyAPI.Application;
 using ReadilyAPI.Application.Exceptions;
 using ReadilyAPI.Application.UseCases.Commands.Wishlist;
 using ReadilyAPI.Application.UseCases.DTO.Wishlists;
 using ReadilyAPI.DataAccess;
+using ReadilyAPI.Domain;
 using ReadilyAPI.Implementation.Validators;
 using System;
 using System.Collections.Generic;
@@ -18,11 +20,13 @@ namespace ReadilyAPI.Implementation.UseCases.Commands.Wishlist
     {
         private readonly IApplicationActor _actor;
         private readonly CreateWishlistValidator _validator;
+        private readonly IMapper _mapper;
 
-        public EfCreateWishlistCommand(ReadilyContext context, IApplicationActor actor, CreateWishlistValidator validator) : base(context)
+        public EfCreateWishlistCommand(ReadilyContext context, IApplicationActor actor, CreateWishlistValidator validator, IMapper mapper) : base(context)
         {
             _actor = actor;
             _validator = validator;
+            _mapper = mapper;
         }
 
         private EfCreateWishlistCommand() { }
@@ -35,16 +39,14 @@ namespace ReadilyAPI.Implementation.UseCases.Commands.Wishlist
         {
             _validator.ValidateAndThrow(data);
 
+            data.UserId = _actor.Id;
+
             if (Context.Users.Include(x => x.Wishlist).First(x=> x.Id == _actor.Id).Wishlist.Any(x => x.Id == data.BookId)) 
             {
                 throw new ConflictException("Book already in a wishlist.");
             }
 
-            Context.Wishlists.Add(new Domain.Wishlist
-            {
-                UserId = _actor.Id,
-                BookId = data.BookId,
-            });
+            Context.Wishlists.Add(_mapper.Map<Domain.Wishlist>(data));
 
             Context.SaveChanges();
         }
